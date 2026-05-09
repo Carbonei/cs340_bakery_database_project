8262a7a55525e4667f046b045b7ec117e22a2796
Asmita Narayanan <narayaas@oregonstate.edu>
2026-05-08 19:29:34 -0700

    Adjusted format for Items Page and reset Port Number

--- project_pages/app.js
+++ project_pages/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const PORT = 1269;
+const PORT = 1268;
 
 // Database
 const db = require('./database/db-connector');
